0f564ce86da8e9cbb860e3fa53e0174fd5a04fe3
Kadir <115056694+KFiloz@users.noreply.github.com>
2025-04-14 18:39:10 -0500

    feat: Ajustando tamaño logo de Ktronika con link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,22 @@
 "use client";
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar: React.FC = () => {
   return (
     <header>
       <nav className="fixed z-50 p-4 w-full border-b border-solid bg-white">
         <div className="flex justify-between items-center mx-auto my-0 max-w-[1200px]">
-          <div className="w-[130px] h-auto">
+          <Link href="/" className="w-[150px] h-[40px] block">
             <Image
               src="/images/Ktronika.png"
-              alt="Ktronika Logo"
-              width={130}
+              alt="Ktronika"
+              width={150}
               height={40}
               priority
             />
-          </div>
+          </Link>
           <ul className="flex gap-8 max-sm:hidden">
             <li><a href="#" className="text-gray-800">Inicio</a></li>
             <li><a href="#" className="text-gray-800">Servicios</a></li>
